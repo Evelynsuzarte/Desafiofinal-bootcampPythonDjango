4844cb487e416b6fa194714761e3cc225f7358f5
Julia Chaves <juliachavess@gmail.com>
2024-10-22 15:31:29 -0300

    Criando busca de noticias

--- plataforma/urls.py
+++ plataforma/urls.py
@@ -6,4 +6,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('home/', views.home, name = 'home'),
     path('home/criar_artigo/', views.criar_artigo, name='criar_artigo'),
+    path('buscar/', views.buscar_noticias, name='buscar_noticias'), 
+
 ]
