c17421cb4d3399de7bdc13667b5988baa20215c0
milenamuch <milenamuchenski@gmail.com>
2024-10-29 13:38:36 -0300

    [fix]adicionando o site ao trusted origins

--- portal_noticias/settings.py
+++ portal_noticias/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-o6-bd@@r%m2!70wuhcv$dso!a=ie#k*v4v9y=c2l=dr*n*i*^_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 #ALLOWED_HOSTS = ['kjnews.onrender.com', 'localhost']
 ALLOWED_HOSTS = []
