c8723275014e9aec7e9ca7f151b5ae61888bd63f
milenamuch <milenamuchenski@gmail.com>
2024-10-28 22:39:55 -0300

    [scripts] Inserts em Categoria

--- plataforma/apps.py
+++ plataforma/apps.py
@@ -4,3 +4,11 @@ from django.apps import AppConfig
 class PlataformaConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'plataforma'
+class PlataformaConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'plataforma'
+
+    def ready(self):
+        print("Iniciando configuração da plataforma...")
+        from .scripts.setup_inicial import setup_inicial
+        setup_inicial()
